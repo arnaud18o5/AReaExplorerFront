77b2324b247b196183ec5062508ed439f8434d46
Arnaud Avocat Gros <a.avocatg@gmail.com>
2021-12-15 12:06:42 +0200

    Problem with cookies fixed

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ export default function App(){
     useEffect(() => {
         if(cookies.username === undefined){
             setLoggedInUser('false')
+            setCookie("isLogged", false, {path:'/'});
         }
         else{
             setLoggedInUser('true')
